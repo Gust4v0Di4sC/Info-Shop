016f03766e1fa8b43ac9264379871c9fdd85646c
Gustavo dias <dscharraa@gmail.com>
2025-08-27 19:55:33 -0300

    feat: Remove a pasta environments do rastreamento do git

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://yvzunraevmocmyiqvzrv.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2enVucmFldm1vY215aXF2enJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NDk3MDksImV4cCI6MjA3MDUyNTcwOX0.elEpWdn9C__GHFRqqh1N1KLrRehafAgJ1dY2bGRcQlo'
-};
