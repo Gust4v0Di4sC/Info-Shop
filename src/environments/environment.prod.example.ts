527e9ecba3065358a7fc80f0452c3b734e5c8b0e
Gustavo dias <dscharraa@gmail.com>
2026-08-16 20:29:07 -0300

    chore: configure sentry dsn defaults

--- src/environments/environment.prod.example.ts
+++ src/environments/environment.prod.example.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   supabaseUrl: '',
   supabaseAnonKey: '',
-  sentryDsn: '',
+  sentryDsn: 'https://2aceb83b47311ad802080f37f0041e80@o4510791290126336.ingest.de.sentry.io/4511915797708880',
   sentryEnvironment: 'production',
   sentryRelease: '',
   sentryTracesSampleRate: 0.2,
